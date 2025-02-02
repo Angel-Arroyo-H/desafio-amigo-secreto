95d009d984e165972fc7844b5ea42f778e7da440
Angel Arroyo <angel.arroyo.h@gmail.com>
2025-02-02 20:18:40 -0300

    Cria uma lista de nomes. Insere nomes validos à lista.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+function adicionarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    validaEntrada(amigo);
+    limpaCampo();
+    console.log(listaDeAmigos); 
+}
+
+function validaEntrada(amigo){
+    // valida que o nome inserido não seja vazio
+    if(amigo.trim() === ''){
+        alert('Entrada inválida: Digite um nome.');
+    }else{
+        listaDeAmigos.push(amigo);        
+        // Podemos adicionar confirmação caso o nome seja um possivel número
+    }
+}
+
+function limpaCampo(){
+    campo = document.getElementById('amigo');
+    campo.value = '';
+}
